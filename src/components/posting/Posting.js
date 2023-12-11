3b2b8e8f6a3657c3392bdc70080910ae932ed409
lsj0202 <w1004123@gmail.com>
2023-12-11 13:19:30 +0900

    feat: add image-upload

--- src/components/posting/Posting.js
+++ src/components/posting/Posting.js
@@ -1,64 +1,129 @@
-import Footer from '../footer/Footer.js';
-import Header from '../header/Header.js';
-import * as a from './StyledPosting.js';
+import React, { useCallback, useRef, useState } from "react";
+import Footer from "../footer/Footer.js";
+import Header from "../header/Header.js";
+import * as a from "./StyledPosting.js";
+import foodBox from "../../apis/instance.js";
+import { styled } from "styled-components";
 
 function Posting() {
+  const inputRef = useRef(null);
+  const [image, setImage] = useState("");
+
+  const onUploadImage = useCallback((e) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+
+    foodBox
+      .post("/food/image", formData)
+      .then((response) => {
+        setImage(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const onImageClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  console.log("image", image.url);
+
   return (
     <div>
-        <Header/>
-        <a.ContentWrap>
-            <a.TitleWrap>
-                <a.Title placeholder='제목을 작성해 주세요'/>
-            </a.TitleWrap>
-            <a.MainContent>
-                <a.MainImg src={require('../../images/addImg.png')}/>
-                <a.InfoWrap>
-                    <a.Info>
-                        <a.IconWrap>
-                            <a.Icon src={require('../../images/one.png')}/>
-                            <a.IconInfo>1인분</a.IconInfo>
-                        </a.IconWrap>
-                        <a.IconWrap>
-                            <a.Icon src={require('../../images/time.png')}/>
-                            <a.IconInfoInput placeholder='소요시간'></a.IconInfoInput>
-                        </a.IconWrap>
-                        <a.IconWrap>
-                            <a.Icon src={require('../../images/nomal.png')}/>
-                            <a.IconInfo>평범함</a.IconInfo>
-                        </a.IconWrap>
-                    </a.Info>
-                    <a.Ingredients>
-                        <a.AWrap>
-                            <a.A>재료</a.A>
-                        </a.AWrap>
-                        <a.BWrap>
-                            <a.CWrap>
-                                <a.Plus src={require('../../images/plus.png')}/>
-                                <a.C placeholder='재료를 추가해주세요.'/>
-                            </a.CWrap>
-                            <a.Link src={require('../../images/link.png')}/>
-                            <a.B placeholder='구매링크를 입력해주세요.'/>
-                        </a.BWrap>
-                    </a.Ingredients>
-                </a.InfoWrap>
-            </a.MainContent>
-            <a.Recipe>
-                <a.T>레시피</a.T>
-                <a.List>
-                    <a.ListTextWrap>
-                        <a.ListTextT placeholder='1. 내용을 추가해주세요.'/>
-                        <a.ListTextC placeholder='내용을 추가해주세요.'/>
-                    </a.ListTextWrap>
-                    <a.ListImg src={require('../../images/addImg2.png')}/>
-                </a.List>
-                <a.AddList src={require('../../images/addList.png')}/>
-                <a.Comment placeholder='팁을 추가해주세요'/>
-            </a.Recipe>
-            <a.Upload>식단 공유</a.Upload>
-        </a.ContentWrap>
-        <Footer/>
+      <Header />
+      <a.ContentWrap>
+        <a.TitleWrap>
+          <a.Title placeholder="제목을 작성해 주세요" />
+        </a.TitleWrap>
+        <a.MainContent>
+          {image.url && <Image src={image.url} alt="img" />}
+          {!image.url && (
+            <>
+              <a.MainImg
+                src={require("../../images/addImg.png")}
+                onClick={onImageClick}
+              />
+              <label htmlFor="fileInput">
+                <Input
+                  id="fileInput"
+                  type="file"
+                  accept="image/*"
+                  ref={inputRef}
+                  onChange={onUploadImage}
+                  style={{ display: "none" }}
+                />
+              </label>
+            </>
+          )}
+          <a.InfoWrap>
+            <a.Info>
+              <a.IconWrap>
+                <a.Icon src={require("../../images/one.png")} />
+                <a.IconInfo>1인분</a.IconInfo>
+              </a.IconWrap>
+              <a.IconWrap>
+                <a.Icon src={require("../../images/time.png")} />
+                <a.IconInfoInput placeholder="소요시간"></a.IconInfoInput>
+              </a.IconWrap>
+              <a.IconWrap>
+                <a.Icon src={require("../../images/nomal.png")} />
+                <a.IconInfo>평범함</a.IconInfo>
+              </a.IconWrap>
+            </a.Info>
+            <a.Ingredients>
+              <a.AWrap>
+                <a.A>재료</a.A>
+              </a.AWrap>
+              <a.BWrap>
+                <a.CWrap>
+                  <a.Plus src={require("../../images/plus.png")} />
+                  <a.C placeholder="재료를 추가해주세요." />
+                </a.CWrap>
+                <a.Link src={require("../../images/link.png")} />
+                <a.B placeholder="구매링크를 입력해주세요." />
+              </a.BWrap>
+            </a.Ingredients>
+          </a.InfoWrap>
+        </a.MainContent>
+        <a.Recipe>
+          <a.T>레시피</a.T>
+          <a.List>
+            <a.ListTextWrap>
+              <a.ListTextT placeholder="1. 내용을 추가해주세요." />
+              <a.ListTextC placeholder="내용을 추가해주세요." />
+            </a.ListTextWrap>
+            <a.ListImg src={require("../../images/addImg2.png")} />
+          </a.List>
+          <a.AddList src={require("../../images/addList.png")} />
+          <a.Comment placeholder="팁을 추가해주세요" />
+        </a.Recipe>
+        <a.Upload>식단 공유</a.Upload>
+      </a.ContentWrap>
+      <Footer />
     </div>
   );
 }
 
 export default Posting;
+
+const Image = styled.img`
+  width: 20rem;
+  height: 25rem;
+  border: 1px solid #d9d9d9;
+  margin-right: 0.5rem;
+`;
+
+const Input = styled.input`
+  width: 20rem;
+  height: 25rem;
+  border: 1px solid #d9d9d9;
+  margin-right: 0.5rem;
+`;
--- src/components/posting/StyledPosting.js
+++ src/components/posting/StyledPosting.js
@@ -1,201 +1,192 @@
 import styled from "styled-components";
 
 export const ContentWrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 1000px;
-    //background-color: pink;
-    margin: auto;
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  //background-color: pink;
+  margin: auto;
 `;
 
 export const Title = styled.input`
-    border:1.5px solid #D9D9D9;
-    padding: 10px;
-    margin-top: 10px;
-    width: 100%;
-    font-size: 20px;
-    font-weight: 600;
-    width: 100%;
+  border: 1.5px solid #d9d9d9;
+  padding: 10px;
+  margin-top: 10px;
+  width: 100%;
+  font-size: 20px;
+  font-weight: 600;
+  width: 100%;
 `;
 
-
 export const TitleWrap = styled.div`
-    border-bottom:1.5px solid #D9D9D9;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 10px;
-    padding-bottom: 10px;
+  border-bottom: 1.5px solid #d9d9d9;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
 `;
 
 export const Date = styled.div`
-    font-size: 14px;
-    margin-top: 3px;
-    color: #D9D9D9;
+  font-size: 14px;
+  margin-top: 3px;
+  color: #d9d9d9;
 `;
 
 export const MainContent = styled.div`
-    display: flex;
-    border-bottom:1.5px solid #D9D9D9;
-
+  display: flex;
+  border-bottom: 1.5px solid #d9d9d9;
 `;
 
 export const MainImg = styled.img`
-    height: 400px;
-    margin-right: 20px;
-`
-export const InfoWrap =styled.div`
-    width: 100%;
-
-`
+  width: 25rem;
+  margin-right: 20px;
+  z-index: 5;
+  height: 25rem;
+`;
+export const InfoWrap = styled.div`
+  width: 100%;
+`;
 export const Info = styled.div`
-    height: 100px;
-    border:1.5px solid #D9D9D9;
-    margin-bottom: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-`
+  height: 100px;
+  border: 1.5px solid #d9d9d9;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
 export const Ingredients = styled.div`
-    padding: 30px;
-    height: 215px;
-    border:1.5px solid #D9D9D9;
-    margin-bottom: 20px;
-`
+  padding: 30px;
+  height: 215px;
+  border: 1.5px solid #d9d9d9;
+  margin-bottom: 20px;
+`;
 export const IconWrap = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const Icon = styled.img `
-    height: 40px;
+export const Icon = styled.img`
+  height: 40px;
 `;
 
 export const IconInfo = styled.div`
-    font-size: 20px;
-    font-weight: 600;
-    margin-left: 10px;
-    color: #575757;
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 10px;
+  color: #575757;
 `;
 
 export const IconInfoInput = styled.input`
-    font-size: 20px;
-    font-weight: 600;
-    margin-left: 10px;
-    color: #575757;
-    border-width: 0;
-    outline: none;
-    width: 100px;
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 10px;
+  color: #575757;
+  border-width: 0;
+  outline: none;
+  width: 100px;
 `;
 
 export const A = styled.div`
-    font-size: 25px;
+  font-size: 25px;
 `;
 
 export const AWrap = styled.div`
-    display: flex;
-    margin-bottom: 5px;
+  display: flex;
+  margin-bottom: 5px;
 `;
 
-export const BWrap = styled.div`
+export const BWrap = styled.div``;
 
-`;
+export const CWrap = styled.div``;
 
-export const CWrap = styled.div`
-
-`;
-
-export const Link = styled.img `
-    height: 10px;
-    margin-left: 20px;
+export const Link = styled.img`
+  height: 10px;
+  margin-left: 20px;
 `;
 
 export const Plus = styled.img`
-    height: 15px;
-    
+  height: 15px;
 `;
 
 export const C = styled.input`
-    border-width: 0;
-    height: 30px;
-    font-size: 20px;
-    outline: none;
-    margin-left: 5px;
+  border-width: 0;
+  height: 30px;
+  font-size: 20px;
+  outline: none;
+  margin-left: 5px;
 `;
 
-export const B= styled.input`
-    border-width: 0;
-    font-size: 15px;
-    padding: 3px;
-    outline: none;
-    color: #6C9BF6;
-    text-decoration: underline;
-    &::placeholder {
-        color: #6C9BF6;
-    }
+export const B = styled.input`
+  border-width: 0;
+  font-size: 15px;
+  padding: 3px;
+  outline: none;
+  color: #6c9bf6;
+  text-decoration: underline;
+  &::placeholder {
+    color: #6c9bf6;
+  }
 `;
 
-export const Recipe = styled.div`
-
-`;
+export const Recipe = styled.div``;
 
 export const T = styled.div`
-    margin-top: 10px;
-    font-weight: 600;
-    margin-bottom: 20px;
+  margin-top: 10px;
+  font-weight: 600;
+  margin-bottom: 20px;
 `;
 
 export const List = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 `;
 
 export const AddList = styled.img`
-    width: 100%;
+  width: 100%;
 `;
 
 export const ListTextWrap = styled.div`
-    margin-left: 30px;
+  margin-left: 30px;
 `;
 
 export const ListTextT = styled.input`
-    font-size: 25px;
-    width: 100%;
-    border-width: 0;
+  font-size: 25px;
+  width: 100%;
+  border-width: 0;
 `;
 
 export const ListTextC = styled.input`
-    color: #575757;
-    margin-left: 25px;
-    width: 100%;
-    border-width: 0;
+  color: #575757;
+  margin-left: 25px;
+  width: 100%;
+  border-width: 0;
 `;
 
 export const ListImg = styled.img`
-    height: 200px;
-    margin-left: 50px;
-    
+  height: 200px;
+  margin-left: 50px;
 `;
 
 export const Comment = styled.input`
-    width: 960px;
-    height: 40px;
-    border:1.5px solid #D9D9D9;
-    font-weight: 600;
-    margin-top: 20px;
-    padding: 20px;
+  width: 960px;
+  height: 40px;
+  border: 1.5px solid #d9d9d9;
+  font-weight: 600;
+  margin-top: 20px;
+  padding: 20px;
 `;
 
 export const Upload = styled.button`
-    background-color: black;
-    border-radius: 30px;
-    color: white;
-    font-size: 15px;
-    font-weight: 700;
-    width: 100px;
-    margin-top: 20px;
-    margin-left: auto;
-    padding: 5px;
+  background-color: black;
+  border-radius: 30px;
+  color: white;
+  font-size: 15px;
+  font-weight: 700;
+  width: 100px;
+  margin-top: 20px;
+  margin-left: auto;
+  padding: 5px;
 `;
